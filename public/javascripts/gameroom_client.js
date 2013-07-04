81539e715bba67ad5eaff9149120747c8b80cfd9
Maximilian Fischer <mafi@inagile.org>
2013-07-04 18:31:03 +0200

    fix ident for gameroom_client

--- public/javascripts/gameroom_client.js
+++ public/javascripts/gameroom_client.js
@@ -1,6 +1,6 @@
-  socket = io.connect("http://192.168.5.38", {port: 5000, transports: ["websocket"]});
-  socket.emit('join');
-  socket.on("player joined", function(data) {
-    console.log(data);
-    $('#players').append(data + '<br />');
-  });
+socket = io.connect("http://192.168.5.38", {port: 5000, transports: ["websocket"]});
+socket.emit('join');
+socket.on("player joined", function(data) {
+  console.log(data);
+  $('#players').append(data + '<br />');
+});
