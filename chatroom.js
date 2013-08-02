a1ec0d3561d931eb2b32e4183c104290a5551d81
Maximilian Fischer <mafi@inagile.org>
2013-08-03 01:11:21 +0200

    Improve webchat

--- chatroom.js
+++ chatroom.js
@@ -1,8 +1,20 @@
 /*chatroom.js
   Realtime chat for game finding*/
 module.exports =  function Chatroom(port){
+  var User = function(uuid,username,id) {
+    var username = username;
+    var uuid = uuid;
+    var id = id;
+    return {
+      uuid: uuid,
+      username: username,
+      id: id
+    }
+  };
+
   var players = [];
   var io = require("socket.io").listen(port);
+  var escape = require('escape-html');
 
   io.configure(function() {
     io.set("transports", ["websocket"]);
@@ -13,15 +25,55 @@ module.exports =  function Chatroom(port){
     io.sockets.on("connection", onSocketConnection);
   };
 
+  function getTimeStamp() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    return  hours + ":" + minutes ;
+  }
+
+  function strip(html) {
+    var tmp = document.createElement("DIV");
+    tmp.innerHTML = html;
+    return tmp.textContent || tmp.innerText || "";
+  }
+
   function onSocketConnection(client) {
+    client.on("disconnect", onClientDisconnect);
     client.on("join", onJoin);
     client.on("getplayers", ongetPlayers);
+    client.on("publicmessage", ongetPublicMessage);
+  };
+
+  function ongetPublicMessage(data) {
+    for(i = 0; i < players.length; i++) {
+      if(players[i].id == this.id) {
+        var message = {
+          timestamp: getTimeStamp(),
+          username: players[i].username,
+          message: escape(data),
+          uuid: players[i].uuid,
+          id: players[i].id
+        }
+        this.broadcast.emit("new publicmessage", message);
+      }
+    }
+    this.emit("message success");
+  };
+
+  function onClientDisconnect() {
+    for(i = 0; i < players.length; i++) {
+      if(players[i].id == this.id) {
+        this.broadcast.emit('remove player', players[i]);
+        players.splice(i, 1);
+      }
+    }
   };
 
   function onJoin(data)  {
-    var user = data;
-    players.push(user);
-    this.broadcast.emit("player joined", user);
+    var new_user = new User(data.uuid, data.username, this.id)
+    players.push(new_user);
+    this.broadcast.emit("player joined", new_user);
   };
 
   function ongetPlayers()  {
--- public/javascripts/chatroom_client.js
+++ public/javascripts/chatroom_client.js
@@ -16,9 +16,10 @@ $(function() {
     }
   };
 
-  var User = function(uuid,username) {
+  var User = function(uuid,username,id) {
     if(uuid != null) {
       var uuid = uuid;
+      var id = id;
     } else {
       var uuid = function() {
         var result ='';
@@ -31,7 +32,8 @@ $(function() {
     var username = username;
     return {
       uuid: uuid,
-      username: username
+      username: username,
+      id: id
     }
   };
 
@@ -48,13 +50,22 @@ $(function() {
     $('#players').append("<div id='"+ my_user.uuid + "'>" + username +"</div>");
   };
 
+  function getTimeStamp() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    return  hours + ":" + minutes ;
+  }
+
   //create own User
   var my_user = new User();
   //get username with session
   getusername();
 
   socket = io.connect("http://127.0.0.1", {port: 5000, transports: ["websocket"]});
+
   socket.emit('getplayers');
+
   socket.on("setplayers", function(data) {
     $.each(data, function(i,player) {
       Users.push(player);
@@ -62,10 +73,46 @@ $(function() {
       $('#players').append("<div id='"+ player.uuid + "'>" + player.username + "</div>");
     });
   });
+
+  socket.on('remove player', function(data) {
+    $("#" + data.uuid).remove();
+    for(i = 0; i < Users.length; i++) {
+      if(Users[i].uuid == data.uuid) {
+        Users.splice(i, 1);
+      }
+    }
+  });
+
   socket.emit('join',my_user);
+
+  socket.on("new publicmessage", function(data) {
+    var message = "<span class='chat_timestamp'>" + data.timestamp  + "</span>" + "<span class='chat_username'>" + data.username + "</span><span class='chat_message'>" + data.message + "</span><br>"
+    $('#chatwindow').append(message);
+    $("#chatwindow").scrollTop($("#chatwindow")[0].scrollHeight);
+  });
+
   socket.on("player joined", function(data) {
     var new_user = new User(data.uuid, data.username);
-    addUser(new_user);
-    $('#players').append("<div id='"+ new_user.uuid + "'>" + new_user.username + "</div>");
+    var usernames = [];
+    $.each(Users, function(i, player) {
+      usernames.push(player.username);
+    });
+    console.log(usernames);
+    if($.inArray(data.username, usernames) == -1) {
+      console.log('test');
+      addUser(new_user);
+      $('#players').append("<div id='"+ new_user.uuid + "'>" + new_user.username + "</div>");
+    };
+  });
+  $('#chatprompt').keypress(function(e){
+    if (e.keyCode == 13 && $("#chatprompt").val() != '') {
+      var public_message = $("#chatprompt").val();
+      console.log(public_message);
+      socket.emit('publicmessage', public_message);
+      $("#chatprompt").val('');
+      var message = "<span class='chat_timestamp'>" + getTimeStamp() + "</span>" + "<span class='chat_username'>" + my_user.username + "</span><span class='chat_message'>" + public_message + "</span><br>"
+      $('#chatwindow').append(message);
+      $("#chatwindow").scrollTop($("#chatwindow")[0].scrollHeight);
+    }
   });
 });
